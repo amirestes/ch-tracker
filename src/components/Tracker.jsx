3be3df351ff2aec8754bdc59097b3c3a2809cc2f
amirestes <amirestes@gmail.com>
2022-06-05 18:17:17 -0500

    added bootstrap progress bar to tracker

--- src/components/Tracker.jsx
+++ src/components/Tracker.jsx
@@ -1,31 +1,15 @@
 import React, { Component } from 'react';
+import { ProgressBar, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Tracker extends Component {
     state = {  } 
     render() { 
         return (
-            <div>
-                <div>
-                    {/* everytime you add points it adds a new div filled in with a background color */}
-                    <table className='table table-bordered'>
-                        <thead>
-                            <tr>
-                                <th>Tracker</th>
-                                <th>tracker</th>
-                                <th>tracker</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>a</td>
-                                <td>b</td>
-                                <td>c</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
+           <div className='progressBar tracker'>
+               <ProgressBar striped variant="" now={33} />
+           </div>
+        )
     }
 }
  
