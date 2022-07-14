300ae7fffeb10b73e0e56d7333746ee38961ebeb
amirestes <amirestes@gmail.com>
2022-07-13 22:54:35 -0500

    created buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import "./styles.css";
+var buttonOne = document.querySelector("#one");
+var buttonTwo = document.querySelector("#two");
+var buttonThree = document.querySelector("#three");
+var buttonFour = document.querySelector("#four");
+var buttonFive = document.querySelector("#five");
+
+let points = 0;
+
+var congrats = function (points) {
+  if (points === 15) {
+    console.log("CONGRATULATIONS ON YOUR NEW CHAMPION!!!");
+  }
+};
+
+congrats(points);
+
+buttonOne.addEventListener("click", function () {});
+
+buttonTwo.addEventListener("click", function () {});
+
+buttonThree.addEventListener("click", function () {});
+
+buttonFour.addEventListener("click", function () {});
+
+buttonFive.addEventListener("click", function () {});
