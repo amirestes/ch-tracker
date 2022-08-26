ceef25f6f371334a9b4eb01072a633c41e025309
amirestes <amirestes@gmail.com>
2022-08-25 23:08:41 -0500

    notes

--- src/index.js
+++ src/index.js
@@ -8,22 +8,20 @@ const increasePoints = (value) => {
   pointsValue.innerHTML = points;
 };
 
-
 const progressChange = () => {
   if (points === 5) {
+    //banner works when I used it with an onclick event and a button, but not working when I put in a conditional
     const banner = document.querySelectorAll("h3");
     banner.innerText = "Hello!!!";
   }
-  
 };
 
 progressChange();
 
-
 // const progressBarUpdater = document.getElementById("progressbar").value;
-  // console.log("test button clicked");
-  // progressBarUpdater.removeAttribute(style);
-  // progressBarUpdater.setAttribute(style, 100);
+// console.log("test button clicked");
+// progressBarUpdater.removeAttribute(style);
+// progressBarUpdater.setAttribute(style, 100);
 // progress bar change class for 5 points
 // const progressUpdate = () => {
 //   if (points === 5) {
