abd3ca912c36a537379a5ecbe22fc5914339ee58
amirestes <amirestes@gmail.com>
2022-10-04 23:10:24 -0500

    updating react

--- React/components/index.js
+++ React/components/index.js
@@ -54,39 +54,5 @@ const reset = () => {
   listName.textContent = "Dog's Name:";
 };
 
-//Controls progress bar changes
-const progressBarUpdater = (points) => {
-  if (points === 1) {
-    progressBarData.style = "width: 7%";
-  } else if (points === 2) {
-    progressBarData.style = "width: 13%";
-  } else if (points === 3) {
-    progressBarData.style = "width: 20%";
-  } else if (points === 4) {
-    progressBarData.style = "width: 27%";
-  } else if (points === 5) {
-    progressBarData.style = "width: 33%";
-  } else if (points === 6) {
-    progressBarData.style = "width: 40%";
-  } else if (points === 7) {
-    progressBarData.style = "width: 47%";
-  } else if (points === 8) {
-    progressBarData.style = "width: 53%";
-  } else if (points === 9) {
-    progressBarData.style = "width: 60%";
-  } else if (points === 10) {
-    progressBarData.style = "width: 66%";
-  } else if (points === 11) {
-    progressBarData.style = "width: 73%";
-  } else if (points === 12) {
-    progressBarData.style = "width: 80%";
-  } else if (points === 13) {
-    progressBarData.style = "width: 87%";
-  } else if (points === 14) {
-    progressBarData.style = "width: 93%";
-  } else if (points >= 15) {
-    progressBarData.style = "width: 100%";
-  }
-};
 
 ReactDom.createRoot(document.getElementById("root").render();
